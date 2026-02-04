e2c6b80755898ac5edb01cbd46a0af53e2c1933d
Milan Pavicevic <lanmi992@live.com>
2026-02-04 09:12:22 +0100

    add type for sailing classes

--- types/entities.ts
+++ types/entities.ts
@@ -9,3 +9,23 @@ export interface OrganisationType {
   email?: string;
   phone?: string;
 }
+
+export interface SailingClassType {
+  id: number;
+  worldSailingId: string;
+  name: string;
+  classCode: string | null;
+  hullType: 'CENTERBOARD' | 'KEELBOAT' | 'KITEBOARDING' | 'MULTIHULL' | 'WINDSURFING';
+  worldSailingStatus: 'INTERNATIONAL' | 'OLYMPIC';
+  numberOfCrew: string | number;
+  numberOfTrapeze: string | number | null;
+  optimalCrewWeight: string | null;
+  hullLength: string | null;
+  beamLength: string | null;
+  boatWeight: string | null;
+  headsailArea: string | null;
+  mainsailArea: string | null;
+  spinnakerArea: string | null;
+  classDesigner: string;
+  yearDesigned: string | null;
+}
