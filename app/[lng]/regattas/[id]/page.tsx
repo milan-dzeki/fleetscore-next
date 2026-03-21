38d12808ecde0c63f59a2d0c3ac986f46348529f
Milan Pavicevic <lanmi992@live.com>
2026-03-21 22:13:52 +0100

    fetch single regatta and render name on regatta page

--- app/[lng]/regattas/[id]/page.tsx
+++ app/[lng]/regattas/[id]/page.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import type { PageWithIdParamsType } from '@/types/props/common';
+import { getRegattaById } from '@/customApi/regattas/regattasApiClient';
+import { getTranslations } from '@/i18n';
+import { REGATTA_PAGE_NS } from '@/i18n/namespaces/pages';
+import PageTitle from '@/components/layout/PageTitle';
+import FormActionMessage from '@/components/forms/FormActionMessage';
 
-const SingleRegattaPage = () => {
+const SingleRegattaPage = async ({
+  params: {
+    lng,
+    id
+  }
+}: PageWithIdParamsType) => {
+  const { t } = await getTranslations(lng, REGATTA_PAGE_NS);
+  const response = await getRegattaById(id);
+
+  if (!response.success) {
+    return (
+      <>
+        <PageTitle title={t('title')} />
+        <FormActionMessage isError message={response.message} />
+      </>
+    );
+  }
+
+  const {
+    name
+  } = response.data;
+
   return (
-    <div>SingleRegattaPage</div>
-  )
-}
+    <>
+      <PageTitle title={name} />
+    </>
+  );
+};
 
-export default SingleRegattaPage
+export default SingleRegattaPage;
--- i18n/namespaces/pages.ts
+++ i18n/namespaces/pages.ts
@@ -13,4 +13,5 @@ export const CREATE_ORGANISATION_PAGE_NS = 'pages/createOrganisation';
 
 export const SAILING_CLASSES_PAGE_NS = 'pages/sailingClasses';
 
+export const REGATTA_PAGE_NS = 'pages/regatta';
 export const CREATE_REGATTA_PAGE_NS = 'pages/createRegatta';
